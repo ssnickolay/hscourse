8cd39df175468de0fd1323acd9fe057f20c4e957
Nikolay Sverchkov <ssnikolay@gmail.com>
2019-03-21 10:23:00 +0300

    Use piplines as a syntaxis

--- eratosthenes_sieve.hs
+++ eratosthenes_sieve.hs
@@ -1,18 +1,27 @@
+-- import HUnit
+import Data.Function
+
 sieve :: Int -> [Int]
-sieve n = sieve' (enumFromTo 2 n)
+sieve n = sieve' [2..n]
+-- [2..n]
+
+-- test1 = TestCase (assertEqual(sieve(5) [2, 3, 5]))
+-- tests = TestList [TestLabel "Main test" test1]
 
 sieve' :: [Int] -> [Int]
 sieve' [] = []
-sieve' (head:tail) = [head] ++ sieve'(sieve'' head tail)
+sieve' (head:tail) = head : sieve' (sieve'' head tail)
 
 sieve'' :: Int -> [Int] -> [Int]
 sieve'' _ [] = []
 sieve'' current (head:tail) = do
   case mod head current of
     0 -> sieve'' current tail
-    _ -> [head] ++ sieve'' current tail
+    _ -> head : sieve'' current tail
 
 main = do
   putStrLn "n="
   n <- getLine
-  putStrLn (show (sieve (read n)))
+  putStrLn $ show $ sieve $ read n
+  -- read n & sieve & show & putStrLn
+  -- putStrLn (show (sieve (read n)))
