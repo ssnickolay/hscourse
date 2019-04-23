c56666f63650e1daeb6231fd39440897b634ba56
Nikolay Sverchkov <ssnikolay@gmail.com>
2019-04-23 22:10:03 +0300

    First menu implementation

--- phonebook.hs
+++ phonebook.hs
@@ -3,8 +3,22 @@ data Entry = Entry { name :: String
                    } deriving(Show)
 
 data Phonebook = Phonebook { entries :: [Entry] } deriving(Show)
+data State = State Int
+
+command :: Phonebook -> State -> IO()
+command phonebook state = do
+  case state of
+    State 0 -> do
+      putStrLn "Please, select a command"
+      putStrLn "0. Help"
+      putStrLn "1. Add new Entry"
+    _ ->
+      putStrLn "Unknown command, try again"
+  input <- getLine
+  command phonebook (State $ read $ input)
 
 main =
   let me = Entry{name = "Kolqa", phone = "+712345" }
       phonebook = Phonebook [me]
-  in putStrLn $ show $ phonebook
+      initState = State 0
+  in command phonebook initState
