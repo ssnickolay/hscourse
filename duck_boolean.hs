e7758a1266e6c2a67ab96549a9a88d24530dce7e
Nikolay Sverchkov <ssnikolay@gmail.com>
2019-04-23 21:13:49 +0300

    Refactor DuckType

--- duck_boolean.hs
+++ duck_boolean.hs
@@ -1,13 +1,21 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 class DuckBoolean a where
   true, false :: a -> Bool
   true a = not $ false a
   false a = not $ true a
 
-
 newtype String2 = String2 String
 
 data Nil = Nil
 data DuckType = DuckTypeS String2 | DuckTypeI Int | DuckTypeN Nil
+
+class DuckTypable a where
+  toDuckType :: a -> DuckType
+
+instance DuckTypable String where
+  toDuckType a = DuckTypeS $ String2 a
 
 instance DuckBoolean String2 where
   true _ = True
@@ -24,12 +32,26 @@ instance DuckBoolean DuckType where
   true (DuckTypeI i) =  true i
   true (DuckTypeN n) =  true n
 
-myIf :: DuckType -> a -> a -> a
-myIf duck right left = if true duck then right else left
+myIf :: DuckTypable b => b -> a -> a -> a
+myIf duck right left = if true (toDuckType duck) then right else left
 
 main = do
-  putStrLn $ show $ (myIf (DuckTypeS $ String2 "foo") 1 0) -- 1
-  putStrLn $ show $ (myIf (DuckTypeS $ String2 "") "Any String is True" "error")
-  putStrLn $ show $ (myIf (DuckTypeI 0) "error" "0 is False")
-  putStrLn $ show $ (myIf (DuckTypeI 100) "100 is True" "error")
-  putStrLn $ show $ (myIf (DuckTypeN Nil) "error" "Nil is False")
+  putStrLn $ show $ (myIf "foo" 1 0)
+  -- putStrLn $ show $ (myIf (DuckTypeS $ String2 "foo") 1 0) -- 1
+  -- putStrLn $ show $ (myIf (DuckTypeS $ String2 "") "Any String is True" "error")
+  -- putStrLn $ show $ (myIf (DuckTypeI 0) "error" "0 is False")
+  -- putStrLn $ show $ (myIf (DuckTypeI 100) "100 is True" "error")
+  -- putStrLn $ show $ (myIf (DuckTypeN Nil) "error" "Nil is False")
+
+{-
+  ДЗ: реализовать тип, повторяющий поведение Ruby Boolean. Нужен сам тип и управляющая конструкция.
+
+  Например,
+
+  myIf "string" 1 2 == 1
+  myIf 0 1 2 == 1
+  myIf Nil 1 2 == 2
+
+  main =
+    myIf "string" (putStrLn "True") (putStrLn "False")
+-}
