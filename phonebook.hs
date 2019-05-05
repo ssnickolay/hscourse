30c442761b067707f7b784a9732dc258463dd39a
Nikolay Sverchkov <ssnikolay@gmail.com>
2019-05-05 19:39:21 +0300

    Raw implementation of spliting phonebook

--- phonebook.hs
+++ phonebook.hs
@@ -9,7 +9,10 @@ data Phonebook = Phonebook { entries :: [Entry] } deriving(Show)
 instance Eq Entry where
   a == b = (name a == name b) && (phone a == phone b)
 
-data State = State Int | InitState
+-- data State = State Int | InitState
+data Command = HelpCommand | RawCommand String
+data Response = PrintHelp
+data State = InitState Phonebook | Wait Phonebook | State Phonebook Int
 
 addEntry :: Phonebook -> String -> String -> Phonebook
 addEntry phonebook name phone =
@@ -31,36 +34,70 @@ deleteEntry phonebook n =
       in phonebook { entries = resultEntries }
     Nothing -> phonebook
 
-command :: Phonebook -> State -> IO()
-command phonebook InitState = do
+-- --- --- COMMANDS --- ---
+-- command :: State -> IO()
+-- command InitState phonebook = do
+--   let lines = ["Please, select a command",
+--                "0. Help",
+--                "1. Add new Entry",
+--                "2. Delete Entry",
+--                "3. Show Phonebook"]
+--   mapM_ putStrLn lines
+--   input <- getLine
+--   command phonebook (State $ read $ input)
+
+-- command State phonebook 1 = do
+--   putStrLn "Fill name:"
+--   name <- getLine
+--   putStrLn "Fill phone:"
+--   phone <- getLine
+--   putStrLn "Entry has been successfully added"
+--   command (addEntry phonebook name phone) InitState
+
+-- command State phonebook 2 = do
+--   putStrLn "Fill name:"
+--   name <- getLine
+--   putStrLn "Entry has been successfully deleted"
+--   command (deleteEntry phonebook name) InitState
+
+-- command State phonebook 3 = do
+--   putStrLn $ show $ phonebook
+--   command phonebook InitState
+
+getCommand :: State -> IO Command
+getCommand (InitState _) = do
+  return HelpCommand
+
+getCommand (Wait _) = do
+  s <- getLine
+  getCommand' $ RawCommand $ head s
+
+getCommand' :: Command -> IO Command
+getCommand' RawCommand "exit" = return Quit
+getCommand' RawCommand "0" = return HelpCommand
+
+handleCommand :: State -> Command -> (State, Response)
+handleCommand (InitState phonebook) HelpCommand =
+  (Wait phonebook, PrintHelp)
+
+handleResponse :: State -> Response -> IO ()
+handleResponse _ PrintHelp = do
   let lines = ["Please, select a command",
                "0. Help",
                "1. Add new Entry",
                "2. Delete Entry",
                "3. Show Phonebook"]
   mapM_ putStrLn lines
-  input <- getLine
-  command phonebook (State $ read $ input)
 
-command phonebook (State 1) = do
-  putStrLn "Fill name:"
-  name <- getLine
-  putStrLn "Fill phone:"
-  phone <- getLine
-  putStrLn "Entry has been successfully added"
-  command (addEntry phonebook name phone) InitState
+loop :: State -> IO()
+loop state = do
+  command <- getCommand state
+  let (newState, response) = handleCommand state command
+  handleResponse newState response
+  loop newState
 
-command phonebook (State 2) = do
-  putStrLn "Fill name:"
-  name <- getLine
-  putStrLn "Entry has been successfully deleted"
-  command (deleteEntry phonebook name) InitState
-
-command phonebook (State 3) = do
-  putStrLn $ show $ phonebook
-  command phonebook InitState
-
+main :: IO ()
 main =
   let me = Entry{name = "Kolqa", phone = "+712345" }
       phonebook = Phonebook [me]
-  in command phonebook InitState
+  in loop $ InitState phonebook
